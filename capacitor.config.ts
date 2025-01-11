3a9e7ec223aac7e99238d8b79321cefdf016a541
2020b-Kordás Patrik <b0korpat@vasvari.org>
2025-01-11 00:11:52 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vasvariApp',
+  webDir: 'dist'
+};
+
+export default config;
